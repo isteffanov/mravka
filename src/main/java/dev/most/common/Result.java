3517ad87b1e671cbf731a836b4b2be7a57e73cb6
Ivan Stefanov <isteffanov@gmail.com>
2025-10-09 17:13:53 +0300

    uses new result monad

--- src/main/java/dev/most/common/Result.java
+++ src/main/java/dev/most/common/Result.java
@@ -1,21 +1,76 @@
 package dev.most.common;
 
-import dev.most.dao.StoreDao;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
-public record Result (ResultType type, Object payload, String message) {
+import static java.util.Objects.nonNull;
+import static java.util.Objects.requireNonNull;
 
-    private static final String DEFAULT_MESSAGE = "No information provided";
+public interface Result<T> {
 
-    public static Result success(Object payload) {
-        return new Result(ResultType.SUCCESS, payload, DEFAULT_MESSAGE);
+    static <T> Result<T> ok(final T value) {
+        requireNonNull(value, "The value of a Result cannot be null");
+        return new Ok<>(value);
     }
 
-    public static Result failure(String reason) {
-        return new Result(ResultType.FAILURE, null, reason);
+    static <T, E extends Throwable> Result<T> error(final E throwable) {
+        requireNonNull(throwable, "The error of a Result cannot be null");
+        return new Error<>(throwable);
     }
 
-    private enum ResultType {
-        SUCCESS,
-        FAILURE
+    static <T> Result<T> of(final Supplier<T> supplier) {
+        requireNonNull(supplier, "The value supplier cannot be null");
+
+        try {
+            return ok(supplier.get());
+        } catch (final Exception error) {
+            return error(error);
+        }
     }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    static <T> Result<T> of(final Optional<T> optional) {
+        requireNonNull(optional, "The optional value cannot be null");
+
+        return optional
+                .map(Result::ok)
+                .orElseGet(() -> error(new NoSuchElementException("No value present when unwrapping the optional")));
+    }
+
+    static <T> Result<T> ofNullable(final T value) {
+        return ofNullable(value, () -> new NullPointerException("The result was initialized with a null value"));
+    }
+
+    static <T> Result<T> ofNullable(final T value, final Supplier<? extends Throwable> errorSupplier) {
+        requireNonNull(errorSupplier, "The error supplier cannot be null");
+
+        return nonNull(value)
+                ? ok(value)
+                : error(errorSupplier.get());
+    }
+
+    boolean isOk();
+
+    void ifOk(final Consumer<T> consumer);
+
+    boolean isError();
+
+    void ifError(final Consumer<Throwable> consumer);
+
+    Result<T> switchIfError(final Function<Throwable, Result<T>> fallbackMethod);
+
+    <U> Result<U> map(final Function<? super T, ? extends U> mapper);
+
+    <U> Result<U> flatMap(final Function<? super T, Result<U>> mapper);
+
+    Result<T> mapError(final Function<Throwable, ? extends Throwable> mapper);
+
+    T get();
+
+    T getOrElse(final Supplier<T> supplier);
+
+    Throwable getError();
 }
--- src/main/java/dev/most/store/StoreManager.java
+++ src/main/java/dev/most/store/StoreManager.java
@@ -19,84 +19,84 @@ public class StoreManager implements IntegerOperations, ListOperations {
         this.store = HashMapBackedStore.create();
     }
 
-    public Result set(String key, String value) {
+    public Result<?> set(String key, String value) {
         store.set(key, StringValue.of(value));
 
-        return Result.success(key);
+        return Result.ok(key);
     }
 
-    public Result get(String key) {
+    public Result<?> get(String key) {
         return store.get(key)
-                .map(Result::success)
-                .orElse(Result.failure("Could not find key."));
+                .map(Result::ok)
+                .orElse(Result.error(new IllegalArgumentException("No such key")));
     }
 
-    public Result del(String key) {
+    public Result<?> del(String key) {
         store.del(key);
 
-        return Result.success(key);
+        return Result.ok(key);
     }
 
-    public Result type(String key) {
+    public Result<?> type(String key) {
         return store.type(key)
-                .map(Result::success)
-                .orElse(Result.failure("Could not find key."));
+                .map(Result::ok)
+                .orElse(Result.error(new IllegalArgumentException("No such key")));
     }
 
     // Integer interface
 
     @Override
-    public Result set(String key, Integer value) {
+    public Result<?> set(String key, Integer value) {
         store.set(key, IntegerValue.of(value));
 
-        return Result.success(key);
+        return Result.ok(key);
     }
 
     @Override
-    public Result increment(String key) {
+    public Result<?> increment(String key) {
         Optional<StoreValue> value = store.get(key);
         if (value.isEmpty()) {
-            return Result.failure("No key");
+            return Result.error(new IllegalArgumentException("No such key"));
         }
 
         if (!(value.get() instanceof IntegerValue integerValue)) {
-            return Result.failure("Not integer");
+            return Result.error(new UnsupportedOperationException("Not an integer"));
         }
 
-        return Result.success(integerValue.increment());
+        return Result.ok(integerValue.increment());
     }
 
     // List interface
 
     @Override
-    public Result prepend(String key, String... values) {
+    public Result<?> prepend(String key, String... values) {
         Optional<StoreValue> value = store.get(key);
 
         if (value.isEmpty()) {
-            return Result.failure("No key");
+            return Result.error(new IllegalArgumentException("No such key"));
         }
 
         if (!(value.get() instanceof ListValue listValue)) {
-            return Result.failure("Not integer");
+            return Result.error(new UnsupportedOperationException("Not an integer"));
         }
 
         listValue.prepend(List.of(values));
-        return Result.success(key);
+        return Result.ok(key);
     }
 
     @Override
-    public Result append(String key, String... values) {
+    public Result<?> append(String key, String... values) {
         Optional<StoreValue> value = store.get(key);
 
         if (value.isEmpty()) {
-            return Result.failure("No key");
+            return Result.error(new IllegalArgumentException("No such key"));
         }
 
         if (!(value.get() instanceof ListValue listValue)) {
-            return Result.failure("Not integer");
+            return Result.error(new UnsupportedOperationException("Not an integer"));
         }
 
         listValue.append(List.of(values));
-        return Result.success(key);
+        return Result.ok(key);
     }
 }
